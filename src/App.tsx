49411a9d454724da24da201f87ebc8c6651a4c88
Ulisses Silverio <ulissessuporteti01@gmail.com>
2022-05-23 20:12:14 -0300

    component tipo lista criado

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Form from './components/form';
+import Lista from './components/lista';
 
 function App() {
   return (
     <div className="App">
       <Form />
+      <Lista />
     </div>
   );
 }
