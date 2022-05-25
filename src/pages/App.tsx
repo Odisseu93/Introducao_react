4cfaba77dd29638932175d401fe7d637a9e6cd92
Ulisses Silverio <ulissessuporteti01@gmail.com>
2022-05-25 20:26:25 -0300

    criado o arquivo style.scss e o estilo do App

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import Form from '../components/form';
 import Lista from '../components/lista';
+import './style.scss'
 
 function App() {
   return (
-    <div className="App">
+    <div className="AppStyle">
       <Form />
       <Lista />
     </div>
