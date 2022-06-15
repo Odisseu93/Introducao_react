909ca3f0ccec2d86b65cd3504ba0347883d3a213
Ulisses Silverio <ulissessuporteti01@gmail.com>
2022-06-14 22:04:11 -0300

    adicionado o component Cronometro

---
+++ src/components/Cronometro/index.tsx
@@ -0,0 +1,14 @@
+import Botao from "../button"
+export function Cronometro() {
+  return (
+    <div>
+      <p>Escolha um card e inicie o cronômetro</p>
+      <div>
+
+      </div>
+      <Botao
+        texto={"Começar!"}
+      />
+    </div>
+  )
+}
