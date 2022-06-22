57a0e212fc295bf56dcc5a966cd7d4597ea67bab
Ulisses Silverio <ulissessuporteti01@gmail.com>
2022-06-21 21:24:28 -0300

    mostrando o relógio

--- src/components/Cronometro/Relogio/index.tsx
+++ src/components/Cronometro/Relogio/index.tsx
@@ -1,7 +1,18 @@
 import React from "react";
 import style from "./Relogio.module.scss";
 
-export  default function Relogio() {
+interface Props {
+  tempo: number | undefined
+}
+
+export  default function Relogio( { tempo  = 0 }: Props ) {
+
+  const minutos = Math.floor (tempo / 60);
+  const segundos = tempo % 60; //retornna o resto
+  const [minutoDezena, minutoUnidade] = String
+  (minutos).padStart(2, '0');
+  const [segundoDezena, segundoUnidade] = String
+  (segundos).padStart(2, '0');
 
 
   // exportar o  component se ter o elemento pai  
@@ -10,18 +21,15 @@ export  default function Relogio() {
     //   <React.Fragment>
     //     <span>0</span>
     //     <span>0</span>
-    //     <span>:</span>
-    //     <span>0</span>
-    //     <span>0</span>
     //   </React.Fragment>
-
+    // ...
     // 2ª forma tag em branco
     <>
-      <span className={style.relogioNumero}>0</span>
-      <span className={style.relogioNumero}>0</span>
+      <span className={style.relogioNumero}>{minutoDezena}</span>
+      <span className={style.relogioNumero}>{minutoUnidade}</span>
       <span className={style.relogioDivisao}>:</span>
-      <span className={style.relogioNumero}>0</span>
-      <span className={style.relogioNumero}>0</span>
+      <span className={style.relogioNumero}>{segundoDezena}</span>
+      <span className={style.relogioNumero}>{segundoUnidade}</span>
     </>
 
   )
